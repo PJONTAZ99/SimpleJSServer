6c2d9777b4109dfceebbf3b72ac9aba19f8e67d1
kampiat <kampiat99@gmail.com>
2023-10-25 20:34:52 +0200

    zmiana napisu

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Grupa ZDTESTpol127 jest super SUPER!');
+  res.end('Grupa ZDTESTpol127 jest super SUPER i MEGA!');
 }
 
 const server = http.createServer(requestListener);
